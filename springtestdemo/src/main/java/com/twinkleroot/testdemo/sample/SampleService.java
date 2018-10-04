e2b3766f3363a5e354d6b3de60e4a9fd72d0493d
AHNJUNGMO\S7 <lalala13@nate.com>
2018-10-04 18:01:20 +0900

    로깅, 테스트 데모

---
+++ springtestdemo/src/main/java/com/twinkleroot/testdemo/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.twinkleroot.testdemo.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "jungmo";
+    }
+}
