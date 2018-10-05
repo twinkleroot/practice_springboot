0b6d5d967329c69f9223a6872ef0dc4ae917560b
AHNJUNGMO\S7 <lalala13@nate.com>
2018-10-05 13:28:42 +0900

    TestRestTemplate 사용(이전 코드는  MockMVC 사용)

--- springtestdemo/src/test/java/com/twinkleroot/testdemo/ApplicationTests.java
+++ springtestdemo/src/test/java/com/twinkleroot/testdemo/ApplicationTests.java
@@ -3,29 +3,22 @@ package com.twinkleroot.testdemo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
     @Autowired
-    MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello jungmo"))
-                .andDo(print());
+        String result = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(result).isEqualTo("hello jungmo");
     }
 }
