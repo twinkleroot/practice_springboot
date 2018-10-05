9e3d41f9d4d1f45c91dd19e0ac6dca07feee1512
AHNJUNGMO\S7 <lalala13@nate.com>
2018-10-05 13:43:54 +0900

    webTestClient 테스트(비동기테스트) 데모

--- springtestdemo/src/test/java/com/twinkleroot/testdemo/ApplicationTests.java
+++ springtestdemo/src/test/java/com/twinkleroot/testdemo/ApplicationTests.java
@@ -1,16 +1,14 @@
 package com.twinkleroot.testdemo;
 
-import com.twinkleroot.testdemo.sample.SampleController;
 import com.twinkleroot.testdemo.sample.SampleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 
@@ -19,8 +17,11 @@ import static org.mockito.Mockito.when;
 // @Runwith 와 같이 사용 해야 함. 빈 설정 @SpringBootApplication 을 찾아가서 알아서 찾는다.
 public class ApplicationTests {
 
+    // 비동기 테스트 : 요청의 응답이 오면 그 때 콜백이 오고 콜백을 실행 할 수 있다.
+    // 웹 클라이언트와 비슷한 api 사용 가능.
+    // 비동기가 아니더라도 이 api가 좋아서라도 webflux 의존성을 추가해서 webClient를 쓸 것 같다.
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     // 테스트가 너무 크기 때문에 @MockBean을 사용해서 쪼갤 수 있음.
     // ApplicationContext에서 SampleService bean을 여기서 만들어준 bean으로 교체한다.
@@ -31,7 +32,9 @@ public class ApplicationTests {
     @Test
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("twinkleroot");
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello twinkleroot");
+
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello twinkleroot");
     }
 }
