473630144825807726cc251a02c4482109ffae72
AHNJUNGMO\S7 <lalala13@nate.com>
2018-10-08 17:44:11 +0900

    Spring HATEOAS 데모

---
+++ springhateoasdemo/src/main/java/com/twinkleroot/springhateoasdemo/SpringhateoasApplication.java
@@ -0,0 +1,11 @@
+package com.twinkleroot.springhateoasdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhateoasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringhateoasApplication.class, args);
+    }
+}
---
+++ springhateoasdemo/src/test/java/com/twinkleroot/springhateoasdemo/SpringhateoasApplicationTests.java
@@ -0,0 +1,4 @@
+package com.twinkleroot.springhateoasdemo;
+
+public class SpringhateoasApplicationTests {
+}
