bfa65a1cf4106a2232100e1809b32f18faafbca7
AHNJUNGMO\S7 <lalala13@nate.com>
2018-10-10 17:58:26 +0900

    Spring data JPA 데모

---
+++ spirngbootjdbc/src/main/java/com/twinkleroot/springbootjdbc/PgSQLRunner.java
@@ -0,0 +1,37 @@
+package com.twinkleroot.springbootjdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class PgSQLRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            System.out.println(dataSource.getClass());
+            System.out.println(connection.getMetaData().getDriverName());
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE account(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (`ID`))";
+            statement.executeUpdate(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO account VALUES(1, 'ahnjungmo')");
+    }
+}
---
+++ spirngbootjdbc/src/test/java/com/twinkleroot/springbootjdbc/SpringbootjdbcApplicationTests.java
@@ -0,0 +1,4 @@
+package com.twinkleroot.springbootjdbc;
+
+public class SpringbootjdbcApplicationTests {
+}
